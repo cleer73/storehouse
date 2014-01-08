dc5583cef1e07e8c742bd3f5eb75acb2b00443ae
Jacob Lee <jake@seomoz.org>
2014-01-08 09:18:24 -0800

    Fleshed out Storehouse\Collection a bit more.

--- src/Storehouse/Collection.php
+++ src/Storehouse/Collection.php
@@ -2,4 +2,25 @@
 
 namespace Storehouse;
 
-class Collection {}
+class Collection {
+
+  private $createRow;
+  private $rows;
+
+  public function __construct($config) {
+    if (isset($config['create_row']) && is_callable($config['create_row'])) {
+      $this->createRow = $config['create_row'];
+    } else {
+      $msg = "A 'create_row' config is required when creating a new collection";
+      throw new \Storehouse\Exception("\Storehouse\Collection::__construct(): {$msg}");
+    }
+  }
+
+  public function add($row) {
+    $this->rows[] = $this->createRow($row);
+  }
+
+  public function __toString() {
+    foreach ($rows as $r) { print $r; }
+  }
+}
